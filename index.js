b4d83ecc94be6f376a3edd349accd15178569f33
Timofei B <timofei.edu@yandex.com>
2018-02-10 01:04:50 +0300

    Инициализация npm проекта

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const args = process.argv.slice(2);
 
+if (args.length === 0) {
+  console.log(`Кекстаграм - инстаграм для котиков
+Автор: Тимофей Б.`);
+  process.exit(0);
+}
+
 switch (args[0]) {
   case `--version`:
-    console.log("v0.0.1");
+    console.log(`v0.0.1`);
     break;
   case `--help`:
     console.log(`Доступные команды:
 --help    — печатает доступные команды;
 --version — печатает версию приложения;`);
-    break;
-  case undefined:
-    console.log(`Кекстаграм - инстаграм для котиков
-Автор: Тимофей Б.`);
     break;
   default:
     console.error(`Неизвестная команда "${args[0]}".
